f0fcfa68cec5692b973b9b32cc04be6912567048
Daniel Cortijo <dcortijo@ucm.es>
2020-05-05 18:53:35 +0200

    Puesto mi proyecto

---
+++ Daniel Cortijo/InGameMap/InGameModel.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InGameMap
+{
+    public class InGameVehicle
+    {
+        public enum vehicleType { plane, ship, car, truck, heli };  
+        public enum weaponType { gun, harpoon, axe};
+        public enum aligment { yours, ally, enemy };
+
+        public int Id { get; set; }
+        public string Imagen { get; set; }
+
+        public vehicleType type;
+        public weaponType weapon;
+        public aligment team;
+
+        public int maxHealth;
+        public int health;
+
+        public int maxOverheat;
+        public int overheat;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool selected = false;
+
+        public bool bomb;
+
+        public InGameVehicle() { }
+    }
+
+    public class Model
+    {
+        public static List<InGameVehicle> Vehicles = new List<InGameVehicle>()
+        {
+            new InGameVehicle()
+            {
+                Id = 0,
+                Imagen = "Assets\\jetYou.png",
+
+                type = InGameVehicle.vehicleType.plane,
+                weapon = InGameVehicle.weaponType.harpoon,
+                team = InGameVehicle.aligment.yours,
+
+                maxHealth = 100,
+                health = 50,
+
+                maxOverheat = 15,
+                overheat = 7,
+
+                X = 250,
+                Y = 150,
+
+                bomb =  true,
+             },
+            new InGameVehicle()
+            {
+                Id = 1,
+                Imagen = "Assets\\carYou.png",
+
+                type = InGameVehicle.vehicleType.car,
+                weapon = InGameVehicle.weaponType.gun,
+                team = InGameVehicle.aligment.yours,
+
+                maxHealth = 150,
+                health = 150,
+
+                maxOverheat = 30,
+                overheat = 30,
+
+                X = 480,
+                Y = 160,
+
+                bomb = false,
+             },
+            new InGameVehicle()
+            {
+                Id = 2,
+                Imagen = "Assets\\shipYou.png",
+
+                type = InGameVehicle.vehicleType.truck,
+                weapon = InGameVehicle.weaponType.gun,
+                team = InGameVehicle.aligment.yours,
+
+                maxHealth = 200,
+                health = 160,
+
+                maxOverheat = 30,
+                overheat = 0,
+
+                X = 140,
+                Y = 500,
+
+                bomb = false,
+             },
+            new InGameVehicle()
+            {
+                Id = 3,
+                Imagen = "Assets\\shipYou.png",
+
+                type = InGameVehicle.vehicleType.heli,
+                weapon = InGameVehicle.weaponType.axe,
+                team = InGameVehicle.aligment.yours,
+
+                maxHealth = 100,
+                health = 25,
+
+                maxOverheat = 10,
+                overheat = 2,
+
+                X = 610,
+                Y = 110,
+
+                bomb = false,
+             },
+            new InGameVehicle()
+            {
+                Id = 4,
+                Imagen = "Assets\\shipAlly.png",
+
+                type = InGameVehicle.vehicleType.ship,
+                weapon = InGameVehicle.weaponType.harpoon,
+                team = InGameVehicle.aligment.ally,
+
+                maxHealth = 200,
+                health = 80,
+
+                maxOverheat = 0,
+                overheat = 0,
+
+                X = 300,
+                Y = 500,
+
+                bomb = false,
+             },
+            new InGameVehicle()
+            {
+                Id = 5,
+                Imagen = "Assets\\carEnemy.png",
+
+                type = InGameVehicle.vehicleType.car,
+                weapon = InGameVehicle.weaponType.harpoon,
+                team = InGameVehicle.aligment.enemy,
+
+                maxHealth = 100,
+                health = 15,
+
+                maxOverheat = 15,
+                overheat = 7,
+
+                X = 130,
+                Y = 130,
+
+                bomb = false,
+             },
+            new InGameVehicle()
+            {
+                Id = 6,
+                Imagen = "Assets\\carEnemy.png",
+
+                type = InGameVehicle.vehicleType.car,
+                weapon = InGameVehicle.weaponType.harpoon,
+                team = InGameVehicle.aligment.enemy,
+
+                maxHealth = 100,
+                health = 50,
+
+                maxOverheat = 15,
+                overheat = 7,
+
+                X = 110,
+                Y = 330,
+
+                bomb = false,
+             }
+
+          };
+
+
+        public static IList<InGameVehicle> GetAllVehicles()
+        {
+            return Vehicles;
+        }
+
+        public static InGameVehicle GetVehicleById(int id)
+        {
+            return Vehicles[id];
+        }
+    }
+}
---
+++ Daniel Cortijo/InGameMap/InGameViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace InGameMap
+{
+    public class InGameVehicleViewModel : InGameVehicle
+    {
+        public Image Img;
+        public ContentControl CCImg;
+        public RotateTransform Rotacion;
+        public int Angulo = 0;
+
+        public InGameVehicleViewModel(InGameVehicle vehicle)
+        {
+            Id = vehicle.Id;
+            Imagen = vehicle.Imagen;
+
+            type = vehicle.type;
+            weapon = vehicle.weapon;
+            team = vehicle.team;
+
+            maxHealth = vehicle.maxHealth;
+            health = vehicle.maxHealth;
+
+            maxOverheat = vehicle.maxOverheat;
+            overheat = vehicle.overheat;
+
+            X = vehicle.X;
+            Y = vehicle.Y;
+
+            Img = new Image();
+            string s = System.IO.Directory.GetCurrentDirectory() + "\\" + vehicle.Imagen;
+            Img.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+            Img.Width = 50;
+            Img.Height = 50;
+
+            CCImg = new ContentControl();
+            CCImg.Content = Img;
+            CCImg.UseSystemFocusVisuals = true;
+            //CCImg.Visibility = Windows.UI.Xaml.Visibility.Visible;//.Collapsed;
+            //Mapa.Children.Add(CCImg);
+            //Mapa.Children.Last().SetValue(Canvas.LeftProperty, X - 25);
+            //Mapa.Children.Last().SetValue(Canvas.TopProperty, Y - 25);
+            Rotacion = new RotateTransform();
+            Rotacion.Angle = Angulo;
+            Rotacion.CenterX = 25;
+            Rotacion.CenterY = 25;
+        }
+    }
+}
