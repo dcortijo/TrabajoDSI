33bc7d8c37a5881db2bb2d7145c64be641c72c04
Daniel Cortijo <dcortijo@ucm.es>
2020-05-21 16:25:01 +0200

    ahhbagfavaf

--- Battlefold 7/Hito3/PlanningView.xaml.cs
+++ Battlefold 7/Hito3/PlanningView.xaml.cs
@@ -25,7 +25,7 @@ namespace Hito3
     {
         public ObservableCollection<PlanningViewVehicleVM> ListaVehiculos { get; } = new ObservableCollection<PlanningViewVehicleVM>();
         DispatcherTimer timer;
-        int timeLeft = 20;
+        int timeLeft = 30;
         int selectedVehicle = 0;
 
         public PlanningView()
